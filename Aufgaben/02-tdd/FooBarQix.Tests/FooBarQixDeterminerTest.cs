031f31c2f8af2b33b466e8c0eda4d236ffc477aa
Nicolas von Rotz <nicolas_vonrotz@sluz.ch>
2022-11-02 13:46:15 +0100

    Aufgaben 02 - TDD

--- Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
+++ Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
@@ -1,12 +1,32 @@
+using FluentAssertions;
 using Xunit;
 
 namespace FooBarQix.Tests
 {
     public class FooBarQixDeterminerTest
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(1, "1")]
+        [InlineData(2, "2")]
+        [InlineData(3, "Foo")]
+        [InlineData(4, "4")]
+        [InlineData(5, "Bar")]
+        [InlineData(6, "Foo")]
+        [InlineData(7, "Qix")]
+        [InlineData(8, "8")]
+        [InlineData(9, "Foo")]
+        [InlineData(10, "Bar")]
+        [InlineData(13, "13")]
+        [InlineData(15, "FooBar")]
+        [InlineData(21, "FooQix")]
+        [InlineData(105, "FooBarQix")]
+        public void TestDetermineFunctionShouldReturnCorrectString(int number, string expected)
         {
+            FooBarQixDeterminer fooBarQixDeterminer = new FooBarQixDeterminer();
+
+            string actual = fooBarQixDeterminer.Determine(number);
+
+            actual.Should().Be(expected);
         }
     }
 }
--- Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
+++ Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
@@ -2,6 +2,28 @@
 {
     public class FooBarQixDeterminer
     {
+        public string Determine(int input)
+        {
+            string output = "";
 
+            if (input % 3 == 0)
+            {
+                 output = "Foo";
+            }
+            if (input % 5 == 0)
+            {
+                output += "Bar";
+            }
+            if (input % 7 == 0)
+            {
+                output += "Qix";
+            }
+            if (output == "")
+            {
+                output = input.ToString();
+            }
+
+            return output;
+        }
     }
 }
